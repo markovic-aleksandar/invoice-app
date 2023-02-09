1013c2abd9262aec697aa2215f4546a8a8140dfb
Aleksandar <aleksandar97kg@gmail.com>
2023-02-09 10:20:02 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// create root
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+// init render
+root.render(<App />);
